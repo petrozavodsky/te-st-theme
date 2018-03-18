846fe9c7bc9028883d290ad3e9276f108aa10465
petrozavodsky <petrozavodsky@gmail.com>
2018-03-18 17:56:07 +0300

    add bootstrap

--- functions.php
+++ functions.php
@@ -13,8 +13,27 @@ class TeStTheme
         add_action('after_setup_theme', array(__CLASS__, 'after_setup_theme'));
         do_action('TeStTheme_setup');
         add_action('wp_ajax_TeStTheme_insert_post', [__CLASS__, 'ajax_insert_posts']);
+        add_action('wp_enqueue_scripts', [__CLASS__, 'add_js_css']);
     }
 
+    public static function add_js_css()
+    {
+
+
+        wp_enqueue_style(
+            'TeStTheme-stylesheet',
+            get_stylesheet_uri(),
+            [],
+            self::$version
+        );
+
+        wp_enqueue_style(
+            'bootstrap',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+            [],
+            '4.0.0'
+        );
+    }
 
     public static function ajax_insert_posts()
     {
@@ -24,7 +43,8 @@ class TeStTheme
     }
 
 
-    public static function  after_setup_theme(){
+    public static function after_setup_theme()
+    {
 
 
     }
