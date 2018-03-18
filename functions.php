766034cf36fcdbc2fa32a88786337e4530e431cd
petrozavodsky <petrozavodsky@gmail.com>
2018-03-18 11:01:05 +0300

    add ajax hook  - fix

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ class TeStTheme
     {
         add_action('after_setup_theme', array(__CLASS__, 'after_setup_theme'));
         do_action('TeStTheme_setup');
-        add_action('wp__ajax__TeStTheme_insert_post', [__CLASS__, 'ajax_insert_posts']);
+        add_action('wp_ajax_TeStTheme_insert_post', [__CLASS__, 'ajax_insert_posts']);
     }
 
 
