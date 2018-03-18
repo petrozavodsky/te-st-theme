71bdb2fd81c0e90a63a7e33a32cbaf5f4ba5310e
petrozavodsky <petrozavodsky@gmail.com>
2018-03-18 10:57:24 +0300

    remname hook

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+
+class TeStTheme {
+    public static $version = '1.0.0';
+
+    private function __construct() {}
+
+     public static function run() {
+        add_action( 'after_setup_theme', array( __CLASS__, 'after_setup_theme' ) );
+        do_action( 'TeStTheme_setup' );
+    }
+
+}
+
+TeStTheme::run();
--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php get_header(); ?>
     <div id="primary" class="content__area">
-        <?php do_action('te-st-theme__theme-content-area-before');?>
+        <?php do_action('TeStTheme__theme-content-area-before');?>
         <div class="base__container">
-            <?php do_action('te-st-theme__theme-main-before');?>
+            <?php do_action('TeStTheme__theme-main-before');?>
             <main id="main-content" class="site__main site__width" role="main">
                 <?php
                 if ( have_posts() ) :
                     while ( have_posts() ) : the_post();?>
-                        <?php do_action('te-st-theme__theme-loop__start', get_the_ID() );?>
+                        <?php do_action('TeStTheme__theme-loop__start', get_the_ID() );?>
                         <?php get_template_part( 'templates/index' );?>
-                        <?php do_action('te-st-theme__theme-loop__end', get_the_ID() );?>
+                        <?php do_action('TeStTheme__theme-loop__end', get_the_ID() );?>
                     <?php endwhile;
                 else : ?>
                     <?php get_template_part( 'templates/empty' ); ?>
                 <?php endif;
                 ?>
             </main>
-            <?php do_action('te-st-theme__theme-main-after');?>
+            <?php do_action('TeStTheme__theme-main-after');?>
         </div>
-        <?php do_action('te-st-theme__theme-content-area-after');?>
+        <?php do_action('TeStTheme__theme-content-area-after');?>
     </div>
 <?php get_footer(); ?>
