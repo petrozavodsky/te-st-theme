57fc33243e9b1e0807c6772311d4ede576c98fe2
petrozavodsky <petrozavodsky@gmail.com>
2018-03-18 22:19:24 +0300

    add screenshot

---
+++ templates/empty.php
@@ -0,0 +1,12 @@
+<article <?php post_class(); ?> >
+    <header>
+        <h2 class="mb-0">
+            <?php _e('No books', 'TeStTheme'); ?>
+        </h2>
+    </header>
+
+    <div class="article__content card-text lead mb-auto">
+        <?php _e('You can add them using the form below', 'TeStTheme'); ?>
+    </div>
+
+</article>
